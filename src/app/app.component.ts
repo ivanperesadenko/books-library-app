5db977c29f5a3c5c2c380fda418f0a0cdceb9bd8
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-10-02 23:24:27 +0300

    Basic eslint & prettier configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bl-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'books-library-app';
-}
+export class AppComponent {}
