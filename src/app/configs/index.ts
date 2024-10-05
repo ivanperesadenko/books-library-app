cc868178f5b5c9e79fe5d7d0bcbe5ba0e73a16b5
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-10-06 02:31:32 +0300

    Barrel files for configs and custom-providers

---
+++ src/app/configs/index.ts
@@ -0,0 +1,3 @@
+export * from './mat-datepicker-format-config';
+export * from './mat-tooltip-config';
+export * from './mat-dialog-config';
---
+++ src/app/custom-providers/index.ts
@@ -0,0 +1,4 @@
+export * from './provide-custom-title-strategy';
+export * from './provide-custom-tooltip-default-options';
+export * from './provide-custom-mat-dialog-default-options';
+export * from './provide-custom-mat-date-formats';
